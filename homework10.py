947126caeb831a79e9df779563c64bec52824e17
ang3lstears <tsaivai@mail.ru>
2024-12-13 12:58:42 +0300

    Update homework10.py

--- homework10.py
+++ homework10.py
@@ -22,16 +22,17 @@ res = pd.merge(df1, df2, on='ID')
 print(res)
 print("number 2")
 import matplotlib.pyplot as plt
-df = pd.DataFrame({
-    'A': [1, 2, 3, 4, 5],
-    'B': [2, 4, 6, 8, 10],
-    'C': [1, 1.5, 2, 2.5, 3]
-})
-plt.figure(figsize=(8, 6))
-plt.plot(df['A'], df['B'], label='A vs B', color='b', marker='o')
-plt.xlabel('A')
-plt.ylabel('B')
-plt.title('График зависимости A от B')
-plt.legend()
-plt.grid(True)
+pol = pd.Series({0: 'female',1: 'male',2: 'male',3: 'female',4: 'female',5: 'male',
+  6: 'male',7: 'male',8: 'male',9: 'female',10: 'female',11: 'female',12: 'female',
+  13: 'female',14: 'male',15: 'male',16: 'male',17: 'male',18: 'male',19: 'female',
+  20: 'female',21: 'female',22: 'male',23: 'male',24: 'male',25: 'male',26: 'female',
+  27: 'female',28: 'male',29: 'female',30: 'male',31: 'female',32: 'female',
+  33: 'male',34: 'female'})
+
+x = pol.value_counts().index
+y = pol.value_counts().values
+plt.bar(x, y, color=['pink', 'blue'])
+plt.xlabel('количество учеников') 
+plt.ylabel('пол учеников') 
+plt.title('соотношение студентов')
 plt.show()
